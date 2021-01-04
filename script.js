c6b0de057f705bab8e7d7078111ad1b71f112b4f
MilindGupta-Creator <milindgupta578@gmail.com>
2021-01-04 21:23:16 +0530

    Ball Movement

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
-alert("js activated");
+// alert("js activated");
+
+let x=true;
+let y=true;
 
 let ball = document.querySelector(".ball");
-let ballcord=ball.getBoundingClientRect();
-let balltop = ballcord.top;
-let ballleft = ballcord.left;
+let board = document.querySelector(".board"); // finding the cordinates of the board
+let boardBounds = board.getBoundingClientRect();
+
+function moveBall(){
+    let ballcord=ball.getBoundingClientRect();
+let ballTop = ballcord.top;
+let ballLeft = ballcord.left;
+let ballBottom = ballcord.bottom;
+let ballRight = ballcord.right;
+ 
+// is the ball in the bound
+// handle vertical bound
+// handle Horizontal
+
+if(ballTop<=boardBounds.top || ballBottom>=boardBounds.bottom){
+    //vertically outside
+    y=!y;
+}
+
+if(ballLeft<=boardBounds.left || ballRight>=boardBounds.right){
+    //horizontally outside
+    x=!x;
+}
+
+
+
+
+ball.style.top=y==true?ballTop+4+"px":ballTop-4+"px";
+ball.style.left=x==true?ballLeft+4+"px":ballLeft-4+"px";
+requestAnimationFrame(moveBall);
+
+}
+
+requestAnimationFrame(moveBall);
